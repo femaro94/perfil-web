fff618738a009a78f254e8bb6289c0d4bec1466f
Fernando Rocha <femaro94@gmail.com>
2018-07-24 14:42:42 -0300

    Adicionando Formulario de contato

---
+++ js/Formulario.js
@@ -0,0 +1,9 @@
+function Enviar(){
+  var nome = document.getElementById("Nome");
+
+  if (nome.value != "") {
+    alert("Obrigado "+nome.value+" ! Mas esta área temporarioamente está indisponivel");
+  }else {
+    alert("Por gentileza , cloque seus dados!!");
+  }
+}
